c0721c86f6c85ca491faf386d3667e8a21f0db64
意绮 <fangjun.yfj@alibaba-inc.com>
2017-06-02 16:29:37 +0800

    update

---
+++ src/index.js
@@ -0,0 +1,17 @@
+/**
+ * @file        A fast way of rendering a tiling image
+ * @author      fangjun.yfj
+ */
+
+/**
+ * Tiny.js
+ * @external Tiny
+ * @see {@link http://tinyjs.net/}
+ */
+
+/**
+ * @namespace Tiny.tiling
+ */
+export {default as TilingSprite} from './TilingSprite';
+export {default as TextureTransform} from './TextureTransform';
+export {default as TilingSpriteRenderer} from './webgl/TilingSpriteRenderer';
